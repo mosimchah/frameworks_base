acc22264183a998dd907d1a589a5f3cddd4ff6e9
mosimchah <mosimchah@gmail.com>
2018-02-22 04:03:29 -0500

    Revert "Runtime toggle of navbar"
    This reverts commit 559f01f85915c3c87d487cf70b7ca8064bd4569b.
    
    Change-Id: Ib18d3d8e6f93607c259276975689d25366c535a4

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6394,11 +6394,6 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
-    public boolean needsNavigationBar() {
-        return mPolicy.needsNavigationBar();
-    }
-
-    @Override
     public void sendCustomAction(Intent intent) {
         mPolicy.sendCustomAction(intent);
     }
